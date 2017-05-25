9bcfecf2c396e27dd756aab60f2ea672e3a9efbd
heiby <zhaoolin@126.com>
2017-05-25 16:27:31 +0800

    Add files via upload

---
+++ StarWars/StarWars/StarWars.h
@@ -0,0 +1,19 @@
+//
+//  StarWars.h
+//  StarWars
+//
+//  Created by hezhaolin on 2017/5/25.
+//  Copyright © 2017年 hezhaolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StarWars.
+FOUNDATION_EXPORT double StarWarsVersionNumber;
+
+//! Project version string for StarWars.
+FOUNDATION_EXPORT const unsigned char StarWarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarWars/PublicHeader.h>
+
+
